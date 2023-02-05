a8d2bf31b2272b2cc77ee4f671c63ef79a5564a1
Louis Brent <louisbrent1992@gmail.com>
2023-02-05 08:20:41 -0800

    added to frontend folder

--- Backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-dotenv.config();
-const { PORT } = process.env;
-
-app.listen(PORT, () => {
-  console.log(`Recipe App listening on port ${PORT}`);
-});
