48b4d866656a510e795fc062cbdd7cb12032cbc9
unknown <louisbrent1992@gmail.com>
2023-09-18 11:36:04 -0700

    added googleAuthProvider to index

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,14 +4,17 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./Redux/store";
 import { PersistGate } from "redux-persist/es/integration/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<App />
-		</PersistGate>
-	</Provider>
+	<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+	</GoogleOAuthProvider>
 );
