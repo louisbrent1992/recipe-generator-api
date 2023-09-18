34f514b1e10ebc91635ac7a4c03c97b4ad83e16e
unknown <louisbrent1992@gmail.com>
2023-09-18 11:39:50 -0700

    created user favorites page

---
+++ Frontend/src/Pages/Favorites.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+// Styled components for the favorites page
+const FavoritesPageContainer = styled.div`
+	padding-bottom: 60px;
+`;
+
+const RecipeCard = styled.li`
+	position: relative;
+	border: 1px solid #ccc;
+	padding: 20px;
+	margin-block: 20px;
+	background-color: #fff;
+	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+	display: flex;
+	flex-direction: column;
+`;
+
+const RecipeTitle = styled.h2`
+	font-size: 24px;
+	margin-bottom: 10px;
+`;
+const RecipeSubtitle = styled.h3`
+	margin-bottom: 10px;
+`;
+
+const RecipeIngredients = styled.ul`
+	list-style: none;
+	padding: 0;
+	margin: 0;
+	font-size: 16px;
+	margin-bottom: 20px;
+
+	li::before {
+		content: "• ";
+		color: #4caf50; /* Green bullet point color */
+	}
+`;
+
+const RecipeSteps = styled.ol`
+	padding: 0;
+	margin: 0;
+	font-size: 16px;
+
+	li {
+		margin-bottom: 10px;
+	}
+`;
+
+const RecipeImageContainer = styled.div`
+	position: absolute;
+	top: 0;
+	right: 0;
+	border-radius: 8px;
+	margin-right: 20px;
+	padding-block: 20px;
+	max-width: 250px;
+	max-height: 250px;
+`;
+
+const RecipeImage = styled.img`
+	max-width: 100%; /* Ensure the image doesn't exceed its container */
+	height: auto; /* Maintain aspect ratio */
+	display: block; /* Remove any default inline spacing */
+	margin: 0 auto; /* Center the image horizontally */
+	border-radius: 8px; /* Add rounded corners */
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add a subtle shadow */
+`;
+
+function Favorites() {
+	// Use useSelector to get the user's saved recipes from the Redux store
+	const savedRecipes = useSelector((state) => state.user.savedRecipes);
+
+	return (
+		<FavoritesPageContainer>
+			<Navbar />
+			<h1 style={{ padding: "20px" }}>Your Favorite Recipes</h1>
+			{savedRecipes.length === 0 ? (
+				<p>No favorite recipes saved yet.</p>
+			) : (
+				<ol>
+					{savedRecipes.map((recipe) => (
+						<RecipeCard key={recipe._id}>
+							<RecipeTitle>{recipe.name}</RecipeTitle>
+							<RecipeImageContainer>
+								<RecipeImage src={recipe.img} />
+							</RecipeImageContainer>
+							<RecipeIngredients>
+								<RecipeSubtitle>Ingredients:</RecipeSubtitle>
+								{recipe.ingredients.map((ingredient) => (
+									<li key={ingredient._id}>
+										{ingredient.quantity} {ingredient.unit} {ingredient.name}
+									</li>
+								))}
+							</RecipeIngredients>
+							<RecipeSteps>
+								<RecipeSubtitle>Steps:</RecipeSubtitle>
+								{recipe.steps.map((step) => (
+									<li style={{ marginLeft: "2rem" }} key={step}>
+										{step}
+									</li>
+								))}
+							</RecipeSteps>
+						</RecipeCard>
+					))}
+				</ol>
+			)}
+			<Footer />
+		</FavoritesPageContainer>
+	);
+}
+
+export default Favorites;
