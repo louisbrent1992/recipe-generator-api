a4609df83aaba68d60f1223c5d9c2b7d983c8275
unknown <louisbrent1992@gmail.com>
2023-09-18 11:40:19 -0700

    updated home page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import Ingredients from "../Components/Ingredients";
 import Recipe from "../Components/Recipe";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import styled from "styled-components";
+
+const DashboardContainer = styled.div``;
 
 function Home() {
 	const [loading, setLoading] = useState(false);
+
 	return (
-		<div>
+		<DashboardContainer>
+			<Navbar />
 			<Ingredients setLoading={setLoading} />
 			<Recipe loading={loading} setLoading={setLoading} />
-		</div>
+			<Footer />
+		</DashboardContainer>
 	);
 }
 
