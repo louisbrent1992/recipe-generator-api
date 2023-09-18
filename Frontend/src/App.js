0768621166fb800d4dfbfd7c4cfcbb3cfac7d182
unknown <louisbrent1992@gmail.com>
2023-09-18 11:35:34 -0700

    added frontend packages/updated App with react routing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import { useSelector } from "react-redux";
+import NotFound from "./Pages/NotFound";
+import Favorites from "./Pages/Favorites";
+import Account from "./Pages/Account";
 
 function App() {
-  return <Home />;
+	const user = useSelector((state) => state.user);
+	const userId = user._id;
+
+	return (
+		<Router>
+			<Routes>
+				<Route path="/dashboard" element={<Home />} />
+				<Route path="/" element={<Home />} />
+				<Route path={`/dashboard/${userId}`} element={<Home />} />
+				<Route path={`/dashboard/${userId}/myAccount`} element={<Account />} />
+				<Route
+					path={`/dashboard/${userId}/favorites`}
+					element={<Favorites />}
+				/>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route element={<NotFound />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
