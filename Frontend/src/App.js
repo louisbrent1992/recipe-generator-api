daa9eae7b3377503c1672c7f7aace0b9896637c2
unknown <louisbrent1992@gmail.com>
2023-09-19 19:38:35 -0700

    updated app routing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,22 +10,16 @@ import Account from "./Pages/Account";
 import About from "./Pages/About";
 
 function App() {
-	const user = useSelector((state) => state.user);
-	const userId = user._id;
-
 	return (
 		<Router>
 			<Routes>
+				<Route exact path="/" element={<Home />} />
 				<Route path="/dashboard" element={<Home />} />
-				<Route path="/" element={<Home />} />
-				<Route path={`/dashboard/${userId}`} element={<Home />} />
-				<Route path={`/dashboard/${userId}/myAccount`} element={<Account />} />
-				<Route
-					path={`/dashboard/${userId}/favorites`}
-					element={<Favorites />}
-				/>
-				<Route path={`/dashboard/${userId}/about`} element={<About />} />
-				<Route path={`/dashboard/about`} element={<About />} />
+				<Route path="/dashboard/:userId" element={<Home />} />
+				<Route path="/dashboard/:userId/myAccount" element={<Account />} />
+				<Route path="/dashboard/:userId/favorites" element={<Favorites />} />
+				<Route path="/dashboard/:userId/about" element={<About />} />
+				<Route path="/dashboard/about" element={<About />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="*" element={<NotFound />} />
