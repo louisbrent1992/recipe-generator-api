e712e0e083a3b968e209f33bdeb78a14a755b8ab
unknown <louisbrent1992@gmail.com>
2023-09-23 14:01:02 -0700

    updated user update router/corsoptions

--- Backend/src/Config/corsOptions.js
+++ Backend/src/Config/corsOptions.js
@@ -6,7 +6,6 @@ const allowedOrigins =
 const corsOptions = {
 	origin: (origin, callback) => {
 		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-			console.log("origin allowed");
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
--- Backend/src/Routes/user.router.js
+++ Backend/src/Routes/user.router.js
@@ -12,90 +12,67 @@ const upload = multer({
 	limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
 });
 
-// User avatar upload route with Multer
-router.put(
-	"/user/avatar_upload/:id",
-	upload.single("avatar"),
-	async (req, res) => {
-		const { id } = req.params;
+// User update and avatar upload route with Multer
+router.put("/user/:id", upload.single("avatar"), async (req, res) => {
+	const { id } = req.params;
+	const { name, email, avatar } = req.body;
 
-		// Initialize avatar data to null
-		let avatarData = null;
-
-		// Check if a file was uploaded
-		if (req.file) {
-			// Check the file format (you can add more formats as needed)
-
-			// Attempt to compress and resize the image using sharp
-			try {
-				let outputFormat = req.file.originalname.split(".").pop().toLowerCase();
-				if (!["jpeg", "jpg", "png", "gif"].includes(outputFormat)) {
-					outputFormat = "jpeg"; // Default to JPEG if the format is not supported
-				}
-
-				const compressedImageBuffer = await sharp(req.file.buffer)
-					.resize(200) // Set your desired width (in pixels)
-					.toFormat(outputFormat) // Set the output format based on the file extension
-					.toBuffer();
-
-				const imgUrl = `data:image/${outputFormat};base64,${compressedImageBuffer.toString(
-					"base64"
-				)}`;
-
-				// Set avatar data if image processing was successful
-				avatarData = imgUrl;
-			} catch (sharpError) {
-				console.error("Image Processing Error:", sharpError);
-				return res
-					.status(400)
-					.json({ error: "Invalid image format or processing error." });
-			}
-		}
-
-		try {
-			const userFound = await User.findById(id);
-
-			if (userFound) {
-				const updatedUser = await User.findByIdAndUpdate(
-					id,
-					{ $set: { avatar: avatarData } },
-					{ new: true }
-				);
-
-				res.status(200).json(updatedUser);
-			} else {
-				res.status(404).json({ message: "User not found" });
-			}
-		} catch (err) {
-			res.status(500).json({ message: err.message });
-		}
-	}
-);
-
-// Update a user by ID
-router.put("/user/:userId", async (req, res) => {
 	try {
-		// Extract the userId from the URL parameters
-		const { userId } = req.params;
+		const userFound = await User.findById(id);
 
-		// Extract updated user data from the request body
-		const updatedUserData = req.body;
+		if (userFound) {
+			// Check if a file was uploaded
+			if (req.file) {
+				// Check the file format (you can add more formats as needed)
 
-		// Find and update the user by their ID in the database
-		const updatedUser = await User.findByIdAndUpdate(userId, updatedUserData, {
-			new: true,
-		});
+				// Attempt to compress and resize the image using sharp
+				try {
+					let outputFormat = req.file.originalname
+						.split(".")
+						.pop()
+						.toLowerCase();
+					if (!["jpeg", "jpg", "png", "gif"].includes(outputFormat)) {
+						outputFormat = "jpeg"; // Default to JPEG if the format is not supported
+					}
 
-		// Check if the user exists
-		if (!updatedUser) {
-			return res.status(404).json({ message: "User not found" });
+					const compressedImageBuffer = await sharp(req.file.buffer)
+						.resize(200) // Set your desired width (in pixels)
+						.toFormat(outputFormat) // Set the output format based on the file extension
+						.toBuffer();
+
+					// Set avatar data if image processing was successful
+					let avatarData = `data:image/${outputFormat};base64,${compressedImageBuffer.toString(
+						"base64"
+					)}`;
+
+					const updatedUser = await User.findByIdAndUpdate(
+						id,
+						{ $set: { avatar: avatarData, name: name, email: email } },
+
+						{ new: true }
+					);
+
+					res.status(200).json(updatedUser);
+				} catch (sharpError) {
+					console.error("Image Processing Error:", sharpError);
+					return res
+						.status(400)
+						.json({ error: "Invalid image format or processing error." });
+				}
+			}
+			const updatedUser = await User.findByIdAndUpdate(
+				id,
+				{ $set: { name: name, email: email } },
+
+				{ new: true }
+			);
+
+			res.status(200).json(updatedUser);
+		} else {
+			res.status(404).json({ message: "User not found" });
 		}
-
-		// Return a successful response with the updated user
-		res.status(200).json(updatedUser);
-	} catch (error) {
-		// Handle errors and return an error response
-		res.status(500).json({ error: error.message });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
 	}
 });
 
