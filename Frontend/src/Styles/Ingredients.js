2efee2d2f66b98099307e02e82b353b4fa5797cb
Louis Brent <louisbrent1992@gmail.com>
2023-10-28 11:07:07 -0700

    updated ingredients list styling

--- Frontend/src/Styles/Ingredients.js
+++ Frontend/src/Styles/Ingredients.js
@@ -2,20 +2,21 @@ import styled from "styled-components";
 import { smallMobile } from "../Utilities/mobileResponse";
 
 const FormContainer = styled.div`
-	width: 100%;
+max-width: 400px;
+margin: 0 auto;
 	margin-top: 20px;
 `;
 
 const StyledForm = styled.form`
-	max-width: 400px;
+	width: 100%;
 	margin: 0 auto;
 	padding: 20px;
 	border: 1px solid #ccc;
 	border-radius: 5px;
 	background-color: #f9f9f9;
 	${smallMobile({
-		maxWidth: "90%",
-	})}
+	maxWidth: "90%",
+})}
 `;
 
 const StyledIngredientContainer = styled.div`
@@ -24,21 +25,6 @@ const StyledIngredientContainer = styled.div`
 	align-items: center;
 	margin-bottom: 10px;
 	position: relative;
-`;
-
-const StyledQuantityControl = styled.div`
-	display: flex;
-	align-items: center;
-`;
-
-const StyledQuantityButton = styled.button`
-	background-color: #4caf50;
-	color: #fff;
-	padding: 5px 10px;
-	border: none;
-	border-radius: 50%;
-	cursor: pointer;
-	margin: 0 5px;
 `;
 
 const StyledHeading = styled.h2`
@@ -53,12 +39,25 @@ const StyledInput = styled.input`
 	border-radius: 5px;
 `;
 
+const StyledAddInput = styled(StyledInput)`
+	margin-top: 10px;
+`;
+
+
+const ButtonsContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	`;
+
 const StyledButton = styled.button`
-	padding: 10px 15px;
 	border: none;
+	padding: 10px 20px;
+	width: 130px;
 	border-radius: 5px;
 	cursor: pointer;
 	margin-top: 10px;
+	background-color: #ccc;
+	text-align: center;
 `;
 
 const StyledSubmitButton = styled(StyledButton)`
@@ -66,27 +65,38 @@ const StyledSubmitButton = styled(StyledButton)`
 	color: #fff;
 `;
 
-const StyledRemoveButton = styled.button`
+const StyledRemoveButton = styled(StyledButton)`
 	background-color: #f44336;
 	color: #fff;
 	padding: 7px 13px;
-	border: none;
-	border-radius: 5px;
-	cursor: pointer;
+	width: 100px;
 	position: absolute;
 	margin-right: 3px;
+	margin-top: 0;
 	right: 0;
+`;
+
+const StyledAddButton = styled(StyledButton)`
+	background-color: #ccc;
+	padding: 7px 13px;
+	width: 100px;
+	position: absolute;
+	right: 0;
+	margin-right: 3px;
+
+
 `;
 
 export {
 	StyledForm,
 	StyledHeading,
 	StyledInput,
+	StyledAddInput,
+	ButtonsContainer,
 	StyledButton,
+	StyledAddButton,
 	StyledSubmitButton,
 	StyledRemoveButton,
 	StyledIngredientContainer,
-	StyledQuantityControl, // Export the new component
-	StyledQuantityButton, // Export the new button component
 	FormContainer,
 };
