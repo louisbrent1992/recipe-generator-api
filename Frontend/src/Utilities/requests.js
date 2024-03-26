491209836fb527935463465f61bd617201136132
Louis <louisbrent1992@gmail.com>
2024-03-25 18:48:18 -0700

    minor ui update

--- Frontend/src/Utilities/requests.js
+++ Frontend/src/Utilities/requests.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
 	process.env.NODE_ENV === "production"
-		? "https://recipe-finder-server-v1.onrender.com"
+		? process.env.REACT_APP_DATABASE_URI_PROD
 		: process.env.REACT_APP_DATABASE_URI || "http://localhost:5050";
